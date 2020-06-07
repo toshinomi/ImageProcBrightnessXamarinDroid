913a126e26367965bfb5f9b4aad893d4c41aca72
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2020-06-07 16:19:53 +0900

    プロジェクト ファイルを追加します。

---
+++ Brightness.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+class Brightness
+{
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
+    public Brightness()
+    {
+    }
+
+    /// <summary>
+    /// デスクトラクタ
+    /// </summary>
+    ~Brightness()
+    {
+    }
+
+    /// <summary>
+    /// グレースケールの実行
+    /// </summary>
+    /// <param name="image">ビットマップ</param>
+    /// <returns>グレースケール後のビットマップ</returns>
+    public Bitmap goImageProcessing(Bitmap bitmap, int alpha)
+    {
+        var pixels = new int[bitmap.Width * bitmap.Height];
+        var resultPixels = new int[bitmap.Width * bitmap.Height];
+
+        bitmap.GetPixels(pixels, 0, bitmap.Width, 0, 0, bitmap.Width, bitmap.Height);
+
+        int count = 0;
+        foreach (var argb in pixels)
+        {
+            var color = new Android.Graphics.Color(argb);
+            resultPixels[count++] = Android.Graphics.Color.Argb(color.A * alpha / 100, color.R, color.G, color.B);
+        }
+
+        var mutableBitmap = Bitmap.CreateBitmap(bitmap.Width, bitmap.Height, bitmap.GetConfig());
+        mutableBitmap.SetPixels(resultPixels, 0, mutableBitmap.Width, 0, 0, mutableBitmap.Width, mutableBitmap.Height);
+
+        return mutableBitmap;
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,74 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Graphics;
+using System;
+using System.Threading.Tasks;
+
+namespace ImageProcBrightnessXamarinDroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private ImageView   mImageView;
+        private Bitmap      mBitmap;
+        private Button      mBtnReset;
+        private SeekBar     mSeekBarAlpha;
+        private TextView    mTextViewAlphaValue;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            InitLayout();
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        private void InitLayout()
+        {
+            mImageView = (ImageView)FindViewById(Resource.Id.image);
+            mBitmap = BitmapFactory.DecodeResource(Resources, Resource.Drawable.dog);
+
+            mBtnReset = (Button)FindViewById(Resource.Id.reset);
+            mBtnReset.Click += OnClickBtnReset;
+
+            mSeekBarAlpha = (SeekBar)FindViewById(Resource.Id.alpha_seekBar);
+            mSeekBarAlpha.ProgressChanged += new EventHandler<SeekBar.ProgressChangedEventArgs>(OnProgressChanged);
+
+            mTextViewAlphaValue = (TextView)FindViewById(Resource.Id.alpha_value);
+        }
+
+        /// <summary>
+        /// リセットボタンのクリックイベント
+        /// </summary>
+        /// <param name="s">オブジェクト</param>
+        /// <param name="e">イベントのデータ</param>
+        private void OnClickBtnReset(object s, EventArgs e)
+        {
+            mImageView.SetImageBitmap(mBitmap);
+        }
+
+        /// <summary>
+        /// シークバーのプログレスチェンジイベント
+        /// </summary>
+        /// <param name="s">オブジェクト</param>
+        /// <param name="e">イベントのデータ</param>
+        private async void OnProgressChanged(object s, SeekBar.ProgressChangedEventArgs e)
+        {
+            mTextViewAlphaValue.Text = e.Progress.ToString() + " %";
+            var brightness = new Brightness();
+            var mutableBitmap = await Task.Run(() => brightness.goImageProcessing(mBitmap, e.Progress));
+            mImageView.SetImageBitmap(mutableBitmap.Copy(Bitmap.Config.Argb8888, false));
+        }
+    }
+}
